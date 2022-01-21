ac0bac8a34e7cec8b0cd13e172dbdde4d0e712bb
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-01-20 17:40:35 -0800

    Add error handling to the API request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,16 +7,22 @@ import axios from 'axios';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: 'san diego'
-            }
-        });
-        setResults(response.data.businesses);
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'san diego'
+                }
+            });
+            setResults(response.data.businesses);
+            setErrorMessage('');
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
     }
 
     return (
@@ -26,6 +32,7 @@ const SearchScreen = () => {
                 onTermChange={setTerm} 
                 onTermSubmit={searchApi}
             />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
         </View>
     );
