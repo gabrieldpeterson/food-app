03305be8d68cb9570f61e0facc19d8843d64c2b3
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-01-22 14:55:37 -0800

    Extract the logic out of the Search screen and put it into a hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
-import axios from 'axios';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const searchApi = async (searchTerm) => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san diego'
-                }
-            });
-            setResults(response.data.businesses);
-            setErrorMessage('');
-        } catch (err) {
-            setErrorMessage('Something went wrong');
-        }
-    }
-
-    useEffect(() => {
-        searchApi('restaurant');
-    }, []);
-
+    const [searchApi, results, errorMessage] = useResults();
+    
     return (
         <View>
             <SearchBar 
