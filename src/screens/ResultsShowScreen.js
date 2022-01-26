33773cca9de20ccb9b7ef9d083981a0e6869ecae
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-01-25 16:48:13 -0800

    Finish the individual restaurant page and show the images provided by the yelp API

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Image, Text, FlatList } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id');
 
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+    if (!result) {
+        return null;
+    }
+
     return (
-        <View>
-            <Text>Results Show ${id}</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}>Results Show {result.name} </Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={photo => photo}
+                renderItem={({ item }) => {
+                    return <Image style={styles.imageStyle} source={{ uri: item }}/>
+                }}
+            />
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        alignItems: 'center'
+    },
+    imageStyle: {
+        width: 300,
+        height: 200,
+        borderRadius: 5,
+        marginBottom: 4
+    },
+    titleStyle: {
+        fontSize: 17,
+        marginTop: 10,
+        marginBottom: 10
+    }
+});
 
 export default ResultsShowScreen;
